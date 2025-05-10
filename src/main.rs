fea5efde1a13b49076caecfe7e4800b69c582197
Weihnachtsbaum <140716472+Weihnachtsbaum@users.noreply.github.com>
2025-05-10 13:16:12 +0200

    Add grid

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,53 @@
 #![register_tool(bevy)]
 #![cfg_attr(not(feature = "console"), windows_subsystem = "windows")]
 
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PrimaryWindow};
+
+const CELL_WIDTH: f32 = 50.0;
+const CELL_HEIGHT: f32 = 25.0;
+const LINE_WIDTH: f32 = 1.0;
+const LINE_LEN: f32 = 10000.0;
 
 fn main() -> AppExit {
-    App::new().add_plugins(DefaultPlugins).run()
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run()
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    window: Single<&Window, With<PrimaryWindow>>,
+) {
+    commands.spawn(Camera2d);
+
+    let mesh = Mesh2d(meshes.add(Rectangle::default()));
+    let material = MeshMaterial2d(materials.add(Color::BLACK));
+    let x_cells = (window.size().x / CELL_WIDTH) as i32;
+    let y_cells = (window.size().y / CELL_HEIGHT) as i32;
+
+    for x in -x_cells / 2 - 1..x_cells / 2 + 1 {
+        commands.spawn((
+            mesh.clone(),
+            material.clone(),
+            Transform {
+                translation: Vec3::new(x as f32 * CELL_WIDTH, 0.0, 0.0),
+                scale: Vec3::new(LINE_WIDTH, LINE_LEN, 1.0),
+                ..default()
+            },
+        ));
+    }
+    for y in -y_cells / 2 - 1..y_cells / 2 + 1 {
+        commands.spawn((
+            mesh.clone(),
+            material.clone(),
+            Transform {
+                translation: Vec3::new(0.0, y as f32 * CELL_HEIGHT, 0.0),
+                scale: Vec3::new(LINE_LEN, LINE_WIDTH, 1.0),
+                ..default()
+            },
+        ));
+    }
 }
